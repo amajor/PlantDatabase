e187c857a0912f184622a5c24f154642961ff1d3
Alison Major <alison.major@gmail.com>
2013-03-17 15:45:38 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlantDatabase::Application.config.secret_token = 'ff97a39cc3e292358df6e89e57f0ab4313cf5913a3b138fc1bce17daed485932a7ec78c28317b0672d3168fae92d18413e41df8204f53ad82ef53142ba6d9b5c'
