91988c82d81f6a84b171d861ca76988d486def26
Alison Major <alison.major@gmail.com>
2013-03-19 12:45:12 -0400

    Revert "Use Devise to allow for users"
    This reverts commit dae55d243164eb1bd74d93af643f26abb9913429.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.12'
 gem 'jquery-rails'
-gem 'devise'
 
 group :development, :test do
 	gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 PlantDatabase::Application.routes.draw do
-  devise_for :views
-
-  devise_for :users
-
   get 'contact' => 'pages#contact'
   root :to => 'pages#home'
 
